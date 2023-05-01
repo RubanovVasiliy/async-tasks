c27451ac69e54e4cd668448e4b1caf58e373a839
Rubanov Vasiliy <rubanovas2005@gmail.com>
2023-05-01 08:14:44 +0700

    init

---
+++ async-tasks/Program.cs
@@ -0,0 +1,38 @@
+﻿
+
+namespace async_tasks;
+
+internal abstract class Program
+{
+    static async Task Main()
+    {
+        Console.WriteLine("Нажмите любую кнопку чтобы выйти \n");
+        
+        var cts = new CancellationTokenSource();
+        var tasks = new Task[5];
+
+        for (var i = 0; i < tasks.Length; i++)
+        {
+            var threadNumber = i + 1;
+            
+            tasks[i] = Task.Run(async () =>
+            {
+                var counter = 0;
+
+                while (!cts.IsCancellationRequested)
+                {
+                    counter++;
+                    Console.WriteLine($"{threadNumber} - {counter}");
+
+                    await Task.Delay(1000);
+                }
+            }, cts.Token);
+        }
+
+        Console.ReadKey(true);
+
+        cts.Cancel();
+
+        await Task.WhenAll(tasks);
+    }
+}
