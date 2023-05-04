ce8666ee429badeef6f84144a2acc809b89cb67b
Rubanov Vasiliy <rubanovas2005@gmail.com>
2023-05-04 12:44:10 +0700

    add CancellationToken to Delay for immediatly close

--- async-tasks/Program.cs
+++ async-tasks/Program.cs
@@ -7,15 +7,16 @@ internal abstract class Program
     static async Task Main()
     {
         Console.WriteLine("Нажмите любую кнопку чтобы выйти \n");
-        
+
         var cts = new CancellationTokenSource();
         var tasks = new Task[5];
 
         for (var i = 0; i < tasks.Length; i++)
         {
             var threadNumber = i + 1;
-            
+
             tasks[i] = Task.Run(async () =>
+            
             {
                 var counter = 0;
 
@@ -24,7 +25,7 @@ internal abstract class Program
                     counter++;
                     Console.WriteLine($"{threadNumber} - {counter}");
 
-                    await Task.Delay(1000);
+                    await Task.Delay(5000, cts.Token);
                 }
             }, cts.Token);
         }
